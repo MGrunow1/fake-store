3c8ba8f6d8a5a85cfb446b834a202c68c7f0e407
MGrunow1 <100005756+MGrunow1@users.noreply.github.com>
2022-07-24 17:13:19 -0700

    feat(ProductDetails): Fetch product info

--- src/components/ProductDetailsPage.js
+++ src/components/ProductDetailsPage.js
@@ -1,5 +1,23 @@
-export default function ProductDetailsPage() {
-    return (
-        <></>
-    )
+import { useEffect, useState } from "react";
+
+export default function ProductDetailsPage(props) {
+  const { id } = props;
+  const [isLoading, setIsLoading] = useState(false);
+  const [productInfo, setProductInfo] = useState([]);
+
+  // fetch product information
+  useEffect(() => {
+    async function getProductInfo() {
+      setIsLoading(true);
+      const url = `http://fakestoreapi.com/products/${id}`;
+      const response = await fetch(url);
+      const data = await response.json();
+      setProductInfo(data);
+      setIsLoading(false);
+    }
+    getProductInfo();
+  }, []);
+  return (
+    <></>
+  )
 }
