e7a30948cdf5f1500eaa94231dd89cd058b6072f
MGrunow1 <100005756+MGrunow1@users.noreply.github.com>
2022-07-27 08:43:20 -0700

    feat(Checkout): Calculate total cost

--- src/components/CheckoutPage.js
+++ src/components/CheckoutPage.js
@@ -1,5 +1,22 @@
+import { useContext, useEffect, useState } from "react";
+import { CartContext } from "../contexts/CartContext";
+
 export default function CheckoutPage() {
+    const { cart } = useContext(CartContext);
+    const [totalCost, setTotalCost] = useState(0);
+
+    // add total cost of cart
+    useEffect(() => {
+        let cost = 0;
+        cart.forEach(element => {
+            cost += (element.price * element.quantity);
+        });
+        setTotalCost(cost);
+    }, [cart]);
+  
     return (
-        <></>
+        <>
+        <div>Total cost: ${totalCost.toFixed(2)}</div>
+        </>
     )
 }
