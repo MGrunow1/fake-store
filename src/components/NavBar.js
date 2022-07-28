0b813863600df7923af8a74cc2d7f05936d41669
MGrunow1 <100005756+MGrunow1@users.noreply.github.com>
2022-07-27 18:42:56 -0700

    feat(NavBar): Style active link

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,16 +4,20 @@ import { NavLink } from "react-router-dom";
 import { NavContainer } from "./StyledComponents";
 export default function NavBar() {
     const { theme } = useContext(ThemeContext);
+    function styleByActiveStatus(isActive) {
+        return isActive ? { fontWeight: "bold", textDecoration: "none"}
+        : {fontWeight: "normal", textDecoration: "none"};
+    }
     return (
         <>
         <NavContainer vertical={false}>
-            <NavLink to="/products" style={{textDecoration: "none"}}>
+            <NavLink to="/products" style={({isActive}) => styleByActiveStatus(isActive)}>
                 <div>Products</div>
             </NavLink>
-            <NavLink to="/checkout" style={{textDecoration: "none"}}>
+            <NavLink to="/checkout" style={({isActive}) => styleByActiveStatus(isActive)}>
                 <div>Checkout</div>
             </NavLink>
-            <NavLink to="/cart" style={{textDecoration: "none"}}>
+            <NavLink to="/cart" style={({isActive}) => styleByActiveStatus(isActive)}>
                 <div>Cart</div>
             </NavLink>
         </NavContainer>
