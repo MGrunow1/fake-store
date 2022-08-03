681d4500055c3ff196aaa31d693322dc5f21c907
MGrunow1 <100005756+MGrunow1@users.noreply.github.com>
2022-08-03 15:46:06 -0700

    feat(NotFound): Allow return to home

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,7 +1,24 @@
+import { useContext } from "react";
+import { ThemeContext } from "../contexts/ThemeContext";
+import { Link } from "react-router-dom";
+import { CenteredGroup, CenteredTitle, MediumText, PrimaryButton } from "./StyledComponents";
+
 export default function NotFound() {
+    const { theme } = useContext(ThemeContext);
     return(
-        <>
-        <div>Page not found</div>
-        </>
+        <div style={{height: "70vh"}}>
+            <CenteredTitle>
+                Page Not Found
+            </CenteredTitle>
+            <MediumText>&nbsp;</MediumText>
+            <CenteredGroup>
+                <Link to="/">
+                    <PrimaryButton dark={theme === 'dark'}>
+                        Try going back to the home page
+                </PrimaryButton>
+            </Link>
+        </CenteredGroup>
+        
+        </div>
     )
 }
